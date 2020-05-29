3f9a5c9a85165fc7c8d541e6dfd7d924429a6f6a
dbsasser <danielbsasser@gmail.com>
2020-05-28 21:26:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+   return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function(arg="special") {
+        return `You are ${flair}${arg}${flair}!`
+    }
+}
+
+let Calculator = {
+    add: function(num1, num2) { return num1 + num2 },
+    subtract: function(num1, num2) { return num1 - num2 },
+    multiply: function(num1, num2) { return num1 * num2 },
+    divide: function(num1, num2) { return num1 / num2 }
+}
+
+function actionApplyer(startingNum, fnList) {
+    let a = startingNum
+
+    for (let i = 0; i < fnList.length; i++) {
+        a = fnList[i](a)
+    }
+
+    return a 
+}
